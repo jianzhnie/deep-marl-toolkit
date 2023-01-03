54379164b258061bd6ebde892c2da01b70f452b9
jianzhnie <jianzhnie@126.com>
2023-01-03 16:44:10 +0800

    update

--- scripts/main.py
+++ scripts/main.py
@@ -5,108 +5,22 @@ import time
 from copy import deepcopy
 
 import mmcv
-import numpy as np
 import torch
-from configs.arguments import get_common_args
-from configs.qmix_config import QMixConfig
-from rltoolkit.data.buffer.ma_replaybuffer import EpisodeData, ReplayBuffer
+from rltoolkit.data.buffer.ma_replaybuffer import ReplayBuffer
 from rltoolkit.utils import TensorboardLogger, WandbLogger
 from rltoolkit.utils.logger.logs import get_outdir, get_root_logger
 from smac.env import StarCraft2Env
 from torch.utils.tensorboard import SummaryWriter
 
-from marltoolkit.agent.qmix import QMixAgent
+sys.path.append('../')
+from configs.arguments import get_common_args
+from configs.qmix_config import QMixConfig
+from marltoolkit.agents.qmix_agent import QMixAgent
 from marltoolkit.envs.env_wrapper import SC2EnvWrapper
 from marltoolkit.modules.agents import RNNModel
 from marltoolkit.modules.mixers import QMixerModel
-
-sys.path.append('../../')
-
-
-def run_train_episode(env: StarCraft2Env,
-                      agent: QMixAgent,
-                      rpm: ReplayBuffer,
-                      config: dict = None):
-
-    episode_limit = config['episode_limit']
-    agent.reset_agent()
-    episode_reward = 0.0
-    episode_step = 0
-    terminated = False
-    state, obs = env.reset()
-    episode_experience = EpisodeData(
-        episode_limit=episode_limit,
-        state_shape=config['state_shape'],
-        obs_shape=config['obs_shape'],
-        num_actions=config['n_actions'],
-        num_agents=config['n_agents'],
-    )
-
-    while not terminated:
-        available_actions = env.get_available_actions()
-        actions = agent.sample(obs, available_actions)
-        next_state, next_obs, reward, terminated = env.step(actions)
-        episode_reward += reward
-        episode_step += 1
-        episode_experience.add(state, obs, actions, available_actions, reward,
-                               terminated, 0)
-        state = next_state
-        obs = next_obs
-
-    # fill the episode
-    for _ in range(episode_step, episode_limit):
-        episode_experience.fill_mask()
-
-    episode_data = episode_experience.get_data()
-
-    rpm.store(**episode_data)
-    is_win = env.win_counted
-
-    mean_loss = []
-    mean_td_error = []
-    if rpm.size() > config['memory_warmup_size']:
-        for _ in range(config['update_learner_freq']):
-            batch = rpm.sample_batch(config['batch_size'])
-            loss, td_error = agent.learn(**batch)
-            mean_loss.append(loss)
-            mean_td_error.append(td_error)
-
-    mean_loss = np.mean(mean_loss) if mean_loss else None
-    mean_td_error = np.mean(mean_td_error) if mean_td_error else None
-
-    return episode_reward, episode_step, is_win, mean_loss, mean_td_error
-
-
-def run_evaluate_episode(env: StarCraft2Env,
-                         agent: QMixAgent,
-                         num_eval_episodes=5):
-    eval_is_win_buffer = []
-    eval_reward_buffer = []
-    eval_steps_buffer = []
-    for _ in range(num_eval_episodes):
-        agent.reset_agent()
-        episode_reward = 0.0
-        episode_step = 0
-        terminated = False
-        state, obs = env.reset()
-        while not terminated:
-            available_actions = env.get_available_actions()
-            actions = agent.predict(obs, available_actions)
-            state, obs, reward, terminated = env.step(actions)
-            episode_step += 1
-            episode_reward += reward
-
-        is_win = env.win_counted
-
-        eval_reward_buffer.append(episode_reward)
-        eval_steps_buffer.append(episode_step)
-        eval_is_win_buffer.append(is_win)
-
-    eval_rewards = np.mean(eval_reward_buffer)
-    eval_steps = np.mean(eval_steps_buffer)
-    eval_win_rate = np.mean(eval_is_win_buffer)
-
-    return eval_rewards, eval_steps, eval_win_rate
+from marltoolkit.runners.episode_runner import (run_evaluate_episode,
+                                                run_train_episode)
 
 
 def main():
@@ -114,6 +28,10 @@ def main():
         'cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     config = deepcopy(QMixConfig)
+    common_args = get_common_args()
+    common_dict = vars(common_args)
+    config.update(common_dict)
+
     env = StarCraft2Env(
         map_name=config['scenario'], difficulty=config['difficulty'])
 
@@ -124,9 +42,6 @@ def main():
     config['n_agents'] = env.n_agents
     config['n_actions'] = env.n_actions
 
-    common_args = get_common_args()
-    common_dict = vars(common_args)
-    config.update(common_dict)
     args = argparse.Namespace(**config)
 
     # init the logger before other steps
@@ -170,6 +85,7 @@ def main():
         input_shape=config['obs_shape'],
         n_actions=config['n_actions'],
         rnn_hidden_dim=config['rnn_hidden_dim'])
+
     mixer_model = QMixerModel(
         n_agents=config['n_agents'],
         state_shape=config['state_shape'],
@@ -182,7 +98,7 @@ def main():
         mixer_model=mixer_model,
         n_agents=config['n_agents'],
         double_q=config['double_q'],
-        total_episode=config['total_episode'],
+        total_steps=config['total_steps'],
         gamma=config['gamma'],
         learning_rate=config['learning_rate'],
         min_learning_rate=config['min_learning_rate'],
@@ -195,27 +111,23 @@ def main():
 
     progress_bar = mmcv.ProgressBar(config['memory_warmup_size'])
     while rpm.size() < config['memory_warmup_size']:
-        episode_reward, episode_step, is_win, mean_loss, mean_td_error = run_train_episode(
-            env, qmix_agent, rpm, config)
+        run_train_episode(env, qmix_agent, rpm, config)
         progress_bar.update()
 
+    steps_cnt = 0
     episode_cnt = 0
-    progress_bar = mmcv.ProgressBar(config['total_episode'])
-    while episode_cnt < config['total_episode']:
+    progress_bar = mmcv.ProgressBar(config['total_steps'])
+    while steps_cnt < config['total_steps']:
         episode_reward, episode_step, is_win, mean_loss, mean_td_error = run_train_episode(
             env, qmix_agent, rpm, config)
-        # update episode
-        qmix_agent.global_episode += 1
-        # update target model
-        if qmix_agent.global_episode % qmix_agent.update_target_interval == 0:
-            qmix_agent.update_target()
-            qmix_agent.target_update_count += 1
-        # update exploration
-        qmix_agent.exploration = max(qmix_agent.ep_scheduler.step(),
-                                     qmix_agent.min_exploration)
+        # update episodes and steps
+        episode_cnt += 1
+        steps_cnt += episode_step
+
         # learning rate decay
-        # qmix_agent.learning_rate = max(
-        #     qmix_agent.lr_scheduler.step(1), qmix_agent.min_learning_rate)
+        qmix_agent.learning_rate = max(
+            qmix_agent.lr_scheduler.step(episode_step),
+            qmix_agent.min_learning_rate)
 
         train_results = {
             'env_step': episode_step,
@@ -228,12 +140,11 @@ def main():
             'replay_buffer_size': rpm.size(),
             'target_update_count': qmix_agent.target_update_count,
         }
-        logger.log_train_data(train_results, episode_cnt)
-
         if episode_cnt % config['train_log_interval'] == 0:
             text_logger.info(
                 '[Train], episode: {}, train_win_rate: {:.2f}, train_reward: {:.2f}'
                 .format(episode_cnt, is_win, episode_reward))
+            logger.log_train_data(train_results, steps_cnt)
 
         if episode_cnt % config['test_log_interval'] == 0:
             eval_rewards, eval_steps, eval_win_rate = run_evaluate_episode(
@@ -247,10 +158,9 @@ def main():
                 'rewards': eval_rewards,
                 'win_rate': eval_win_rate
             }
-            logger.log_test_data(test_results, episode_cnt)
+            logger.log_test_data(test_results, steps_cnt)
 
-        episode_cnt += 1
-        progress_bar.update()
+        progress_bar.update(episode_step)
 
 
 if __name__ == '__main__':
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 # main
-nohup python main.py  --scenario 3m  --total_episode 10000 > runoob1.log 2>&1 &
-nohup python main.py  --scenario 8m  --total_episode 10000 > runoob2.log 2>&1 &
-nohup python main.py  --scenario 5m_vs_6m  --total_episode 10000  > runoob3.log 2>&1 &
-nohup python main.py  --scenario 8m_vs_9m  --total_episode 10000  > runoob4.log 2>&1 &
-nohup python main.py  --scenario MMM   --total_episode 10000  > runoob5.log 2>&1 &
-nohup python main.py  --scenario 2s3z  --total_episode 10000  > runoob6.log 2>&1 &
-nohup python main.py  --scenario 3s5z  --total_episode 10000 > runoob7.log 2>&1 &
+nohup python main.py  --scenario 3m  --total_steps 1000000 > runoob1.log 2>&1 &
+nohup python main.py  --scenario 8m  --total_steps 1000000 > runoob2.log 2>&1 &
+nohup python main.py  --scenario 5m_vs_6m  --total_steps 1000000  > runoob3.log 2>&1 &
+nohup python main.py  --scenario 8m_vs_9m  --total_steps 1000000  > runoob4.log 2>&1 &
+nohup python main.py  --scenario MMM   --total_steps 1000000  > runoob5.log 2>&1 &
+nohup python main.py  --scenario 2s3z  --total_steps 1000000  > runoob6.log 2>&1 &
+nohup python main.py  --scenario 3s5z  --total_steps 1000000 > runoob7.log 2>&1 &
