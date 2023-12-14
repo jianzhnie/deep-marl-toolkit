6bed086da1d6a1cda5972a5951c1251c61b8d7f8
jianzhnie <jianzhnie@126.com>
2023-12-14 15:06:30 +0800

    update marltoolkit

--- marltoolkit/utils/logger/__init__.py
+++ marltoolkit/utils/logger/__init__.py
@@ -1,7 +1,9 @@
 from .base import BaseLogger
+from .logs import get_outdir, get_root_logger
 from .tensorboard import TensorboardLogger
 from .wandb import WandbLogger
-from .logs import get_root_logger, get_outdir
 
-
-__all__ = [ 'BaseLogger', 'TensorboardLogger', 'WandbLogger', 'get_root_logger', 'get_outdir' ]
+__all__ = [
+    'BaseLogger', 'TensorboardLogger', 'WandbLogger', 'get_root_logger',
+    'get_outdir'
+]
