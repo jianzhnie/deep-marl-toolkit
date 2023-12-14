6f49eb08c5b0b0a4d1cc0321a82b5e416f2e14e5
jianzhnie <jianzhnie@126.com>
2023-12-14 13:04:08 +0800

    update marltoolkit

--- marltoolkit/utils/__init__.py
+++ marltoolkit/utils/__init__.py
@@ -1,10 +1,16 @@
 from .logger import (BaseLogger, TensorboardLogger, WandbLogger, get_outdir,
                      get_root_logger)
+from .lr_scheduler import (LinearDecayScheduler, MultiStepScheduler,
+                           PiecewiseScheduler)
+from .model_utils import (check_model_method, hard_target_update,
+                          soft_target_update)
 from .progressbar import ProgressBar
 from .timer import Timer
 from .transforms import OneHotTransform
 
 __all__ = [
-    'ProgressBar', 'Timer', 'OneHotTransform', 'BaseLogger',
-    'TensorboardLogger', 'WandbLogger', 'get_root_logger', 'get_outdir'
+    'BaseLogger', 'TensorboardLogger', 'WandbLogger', 'get_outdir',
+    'get_root_logger', 'ProgressBar', 'Timer', 'OneHotTransform',
+    'hard_target_update', 'soft_target_update', 'check_model_method',
+    'LinearDecayScheduler', 'PiecewiseScheduler', 'MultiStepScheduler'
 ]
