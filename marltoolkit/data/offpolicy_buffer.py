b708b65174cc8deb6eb4ae21741553de34a6f69f
jianzhnie <jianzhnie@126.com>
2024-01-12 10:22:48 +0800

    update marltoolkit

--- marltoolkit/data/offpolicy_buffer.py
+++ marltoolkit/data/offpolicy_buffer.py
@@ -115,8 +115,8 @@ class MaEpisodeData(object):
                                       state_space)
 
         # memory management
-        self._curr_ptr = 0
-        self._curr_size = 0
+        self.curr_ptr = 0
+        self.curr_size = 0
 
     def store_episodes(
         self,
@@ -130,26 +130,26 @@ class MaEpisodeData(object):
         filled: np.ndarray,
     ):
         assert self.size() < self.episode_limit
-        self.obs_buf[self._curr_ptr] = obs
-        self.action_buf[self._curr_ptr] = actions
-        self.action_onehot_buf[self._curr_ptr] = actions_onehot
-        self.available_action_buf[self._curr_ptr] = available_actions
-        self.reward_buf[self._curr_ptr] = rewards
-        self.terminal_buf[self._curr_ptr] = terminated
-        self.filled_buf[self._curr_ptr] = filled
+        self.obs_buf[self.curr_ptr] = obs
+        self.action_buf[self.curr_ptr] = actions
+        self.action_onehot_buf[self.curr_ptr] = actions_onehot
+        self.available_action_buf[self.curr_ptr] = available_actions
+        self.reward_buf[self.curr_ptr] = rewards
+        self.terminal_buf[self.curr_ptr] = terminated
+        self.filled_buf[self.curr_ptr] = filled
 
         if self.state_space is not None:
-            self.state_buf[self._curr_ptr] = state
+            self.state_buf[self.curr_ptr] = state
 
-        self._curr_ptr += 1
-        self._curr_size = min(self._curr_size + 1, self.episode_limit)
+        self.curr_ptr += 1
+        self.curr_size = min(self.curr_size + 1, self.episode_limit)
 
     def fill_mask(self):
         assert self.size() < self.episode_limit
-        self.terminal_buf[self._curr_ptr] = True
-        self.filled_buf[self._curr_ptr] = 1.0
-        self._curr_ptr += 1
-        self._curr_size = min(self._curr_size + 1, self.episode_limit)
+        self.terminal_buf[self.curr_ptr] = True
+        self.filled_buf[self.curr_ptr] = 1.0
+        self.curr_ptr += 1
+        self.curr_size = min(self.curr_size + 1, self.episode_limit)
 
     def get_data(self):
         """Get all the data in an episode."""
@@ -167,10 +167,10 @@ class MaEpisodeData(object):
 
     def size(self) -> int:
         """get current size of replay memory."""
-        return self._curr_size
+        return self.curr_size
 
     def __len__(self):
-        return self._curr_size
+        return self.curr_size
 
 
 class OffPolicyBuffer(BaseBuffer):
@@ -198,6 +198,7 @@ class OffPolicyBuffer(BaseBuffer):
         action_space: Union[int, Tuple],
         reward_space: Union[int, Tuple],
         done_space: Union[int, Tuple],
+        device: Union[torch.device, str] = 'cpu',
         **kwargs,
     ):
         super(OffPolicyBuffer, self).__init__(
@@ -209,9 +210,11 @@ class OffPolicyBuffer(BaseBuffer):
             action_space,
             reward_space,
             done_space,
+            device,
         )
 
-        self.max_size = buffer_size // num_envs
+        # Adjust buffer size
+        self.buffer_size = max(buffer_size // num_envs, 1)
         if self.state_space is not None:
             self.store_global_state = True
         else:
@@ -220,67 +223,51 @@ class OffPolicyBuffer(BaseBuffer):
         self.reset_buffer()
         self.buffer_keys = self.buffers.keys()
         # memory management
-        self._curr_ptr = 0
-        self._curr_size = 0
+        self.curr_ptr = 0
+        self.curr_size = 0
 
     def reset_buffer(self):
-        if self.store_global_state:
-            self.state_buf = np.zeros((self.num_envs, self.max_size) +
-                                      self.state_space)
-            self.state_next = np.zeros((self.num_envs, self.max_size) +
-                                       self.state_space)
-        self.obs_buf = np.zeros((self.num_envs, self.max_size) +
-                                self.obs_space)
-        self.obs_next_buf = np.zeros((self.num_envs, self.max_size) +
-                                     self.obs_space)
-        self.action_buf = np.zeros((self.num_envs, self.max_size) +
-                                   self.action_space)
-        self.action_onehot_buf = np.zeros((self.num_envs, self.max_size,
-                                           self.num_agents) +
-                                          self.action_space)
-        self.available_action_buf = np.zeros((self.num_envs, self.max_size,
-                                              self.num_agents) +
-                                             self.action_space)
-        self.agent_mask_buf = np.zeros(
-            (self.num_envs, self.max_size, self.num_agents)).astype(np.bool)
-        self.reward_buf = np.zeros((self.num_envs, self.max_size) +
-                                   self.reward_space)
-        self.terminal_buf = np.zeros((self.num_envs, self.max_size) +
-                                     self.done_space).astype(np.bool)
-        self.filled_buf = np.zeros((self.num_envs, self.max_size) +
-                                   self.done_space).astype(np.bool)
-
         self.buffers = dict(
-            obs=np.zeros((self.num_envs, self.max_size) + self.obs_space),
-            obs_next=np.zeros((self.num_envs, self.max_size) + self.obs_space),
-            actions=np.zeros((self.num_envs, self.max_size) +
-                             self.action_space),
-            actions_onehot=np.zeros((self.num_envs, self.max_size,
-                                     self.num_agents) + self.action_space),
-            available_actions=np.zeros((self.num_envs, self.max_size,
-                                        self.num_agents) + self.action_space),
-            agent_mask=np.zeros((self.num_envs, self.max_size,
+            obs=np.zeros(
+                (self.num_envs, self.buffer_size) + self.obs_space,
+                dtype=np.float32),
+            obs_next=np.zeros(
+                (self.num_envs, self.buffer_size) + self.obs_space,
+                dtype=np.float32),
+            actions=np.zeros(
+                (self.num_envs, self.buffer_size) + self.action_space,
+                dtype=np.int8),
+            actions_onehot=np.zeros(
+                (self.num_envs, self.buffer_size, self.num_agents) +
+                self.action_space,
+                dtype=np.int8),
+            available_actions=np.zeros(
+                (self.num_envs, self.buffer_size, self.num_agents) +
+                self.action_space,
+                dtype=np.int8),
+            agent_mask=np.zeros((self.num_envs, self.buffer_size,
                                  self.num_agents)).astype(np.bool),
-            rewards=np.zeros((self.num_envs, self.max_size) +
+            rewards=np.zeros((self.num_envs, self.buffer_size) +
                              self.reward_space),
-            terminated=np.zeros((self.num_envs, self.max_size) +
+            terminated=np.zeros((self.num_envs, self.buffer_size) +
                                 self.done_space).astype(np.bool),
-            filled=np.zeros((self.num_envs, self.max_size) +
+            filled=np.zeros((self.num_envs, self.buffer_size) +
                             self.done_space).astype(np.bool),
         )
         if self.store_global_state:
-            self.buffers['state'] = np.zeros((self.num_envs, self.max_size) +
-                                             self.state_space)
-            self.buffers['state_next'] = np.zeros((self.num_envs,
-                                                   self.max_size) +
-                                                  self.state_space)
+            self.buffers['state'] = np.zeros(
+                (self.num_envs, self.buffer_size) + self.state_space,
+                dtype=np.float32)
+            self.buffers['state_next'] = np.zeros(
+                (self.num_envs, self.buffer_size) + self.state_space,
+                dtype=np.float32)
 
     def store(self, step_data: Dict[str, np.ndarray]) -> None:
         for k in self.buffer_keys:
             assert k in step_data.keys(), f'{k} not in step_data'
-            self.buffers[k][self._curr_ptr] = step_data[k]
-        self._curr_ptr = (self._curr_ptr + 1) % self.buffer_size
-        self._curr_size = min(self._curr_size + 1, self.buffer_size)
+            self.buffers[k][self.curr_ptr] = step_data[k]
+        self.curr_ptr = (self.curr_ptr + 1) % self.buffer_size
+        self.curr_size = min(self.curr_size + 1, self.buffer_size)
 
     def sample_batch(self, batch_size: int = None) -> Dict[str, np.ndarray]:
         """sample a batch from replay memory.
@@ -291,22 +278,19 @@ class OffPolicyBuffer(BaseBuffer):
         Returns:
             a batch of experience samples: obs, action, reward, next_obs, terminal
         """
-        assert batch_size < self._curr_size, f'Batch Size: {batch_size} is larger than the current Buffer Size:{self._curr_size}'
+        assert batch_size < self.curr_size, f'Batch Size: {batch_size} is larger than the current Buffer Size:{self.curr_size}'
         env_idxs = np.random.randint(self.num_envs, size=batch_size)
-        step_idxs = np.random.randint(self._curr_size, size=batch_size)
+        step_idxs = np.random.randint(self.curr_size, size=batch_size)
 
         batch = {
             key: self.buffers[key][env_idxs, step_idxs]
             for key in self.buffer_keys
         }
+        batch = self.to_torch(batch)
         return batch
 
-    def size(self) -> int:
-        """get current size of replay memory."""
-        return self._curr_size
-
     def __len__(self):
-        return self._curr_size
+        return self.curr_size
 
 
 class OffPolicyBufferRNN(OffPolicyBuffer):
@@ -350,7 +334,7 @@ class OffPolicyBufferRNN(OffPolicyBuffer):
 
     def reset_buffer(self):
         self.buffers = dict(
-            obs=np.zeros((self.num_envs, self.max_size, self.num_agents,
+            obs=np.zeros((self.num_envs, self.buffer_size, self.num_agents,
                           self.max_eps_len) + self.obs_space),
             obs_next=np.zeros((self.num_envs, self.num_agents,
                                self.max_eps_len) + self.obs_space),
@@ -382,9 +366,9 @@ class OffPolicyBufferRNN(OffPolicyBuffer):
     def store(self, step_data: Dict[str, np.ndarray]) -> None:
         for k in self.buffer_keys:
             assert k in step_data.keys(), f'{k} not in step_data'
-            self.buffers[k][self._curr_ptr] = step_data[k]
-        self._curr_ptr = (self._curr_ptr + 1) % self.buffer_size
-        self._curr_size = min(self._curr_size + 1, self.buffer_size)
+            self.buffers[k][self.curr_ptr] = step_data[k]
+        self.curr_ptr = (self.curr_ptr + 1) % self.buffer_size
+        self.curr_size = min(self.curr_size + 1, self.buffer_size)
 
     def sample_batch(self, batch_size: int = None) -> Dict[str, np.ndarray]:
         """sample a batch from replay memory.
@@ -395,9 +379,9 @@ class OffPolicyBufferRNN(OffPolicyBuffer):
         Returns:
             a batch of experience samples: obs, action, reward, next_obs, terminal
         """
-        assert batch_size < self._curr_size, f'Batch Size: {batch_size} is larger than the current Buffer Size:{self._curr_size}'
+        assert batch_size < self.curr_size, f'Batch Size: {batch_size} is larger than the current Buffer Size:{self.curr_size}'
         env_idxs = np.random.randint(self.num_envs, size=batch_size)
-        step_idxs = np.random.randint(self._curr_size, size=batch_size)
+        step_idxs = np.random.randint(self.curr_size, size=batch_size)
 
         batch = {
             key: self.buffers[key][env_idxs, step_idxs]
@@ -407,10 +391,10 @@ class OffPolicyBufferRNN(OffPolicyBuffer):
 
     def size(self) -> int:
         """get current size of replay memory."""
-        return self._curr_size
+        return self.curr_size
 
     def __len__(self):
-        return self._curr_size
+        return self.curr_size
 
 
 class IndependReplayBuffer(object):
@@ -431,8 +415,8 @@ class IndependReplayBuffer(object):
         self.terminal_buf = np.zeros((buffer_size, num_agents),
                                      dtype=np.float32)
 
-        self._curr_ptr = 0
-        self._curr_size = 0
+        self.curr_ptr = 0
+        self.curr_size = 0
         self.obs_dim = obs_dim
         self.num_agents = num_agents
         self.buffer_size = buffer_size
@@ -444,19 +428,19 @@ class IndependReplayBuffer(object):
                               terminal_all):
             obs, act, reward, next_obs, terminal = transition
 
-            self.obs_buf[self._curr_ptr, agent_idx] = obs
-            self.next_obs_buf[self._curr_ptr, agent_idx] = next_obs
-            self.action_buf[self._curr_ptr, agent_idx] = act
-            self.reward_buf[self._curr_ptr, agent_idx] = reward
-            self.terminal_buf[self._curr_ptr, agent_idx] = terminal
+            self.obs_buf[self.curr_ptr, agent_idx] = obs
+            self.next_obs_buf[self.curr_ptr, agent_idx] = next_obs
+            self.action_buf[self.curr_ptr, agent_idx] = act
+            self.reward_buf[self.curr_ptr, agent_idx] = reward
+            self.terminal_buf[self.curr_ptr, agent_idx] = terminal
 
             agent_idx += 1
 
-        self._curr_ptr = (self._curr_ptr + 1) % self.buffer_size
-        self._curr_size = min(self._curr_size + 1, self.buffer_size)
+        self.curr_ptr = (self.curr_ptr + 1) % self.buffer_size
+        self.curr_size = min(self.curr_size + 1, self.buffer_size)
 
     def sample_batch(self, batch_size: int) -> Dict[str, np.ndarray]:
-        idxs = np.random.randint(self._curr_size, size=batch_size)
+        idxs = np.random.randint(self.curr_size, size=batch_size)
 
         batch = dict(
             obs=self.obs_buf[idxs],
@@ -473,7 +457,7 @@ class IndependReplayBuffer(object):
                      batch_size: int) -> Dict[str, np.ndarray]:
 
         start_idx = np.random.randint(
-            self._curr_size - chunk_size, size=batch_size)
+            self.curr_size - chunk_size, size=batch_size)
 
         obs_chunk, next_obs_chunk, action_chunk, reward_chunk, terminal_chunk = [], [], [], [], []
 
@@ -507,10 +491,10 @@ class IndependReplayBuffer(object):
 
     def size(self) -> int:
         """get current size of replay memory."""
-        return self._curr_size
+        return self.curr_size
 
     def __len__(self):
-        return self._curr_size
+        return self.curr_size
 
 
 if __name__ == '__main__':
