ca2345ef063f1b509769953da582b1863fef8592
jianzhnie <jianzhnie@126.com>
2024-01-19 11:15:14 +0800

    update marltoolkit

--- marltoolkit/envs/smac/sc2_env.py
+++ marltoolkit/envs/smac/sc2_env.py
@@ -8,12 +8,14 @@ from copy import deepcopy
 from typing import List, Tuple, Union
 
 import numpy as np
+from gymnasium.spaces import Box, Dict, Discrete
 from smac.env import StarCraft2Env
 
+from marltoolkit.envs.multiagentenv import MARLBaseEnv
 from marltoolkit.utils.transforms import OneHotTransform
 
 
-class SC2Env:
+class MARLSMACEnv(object):
     """Wrapper for StarCraft2Env providing a more user-friendly interface."""
 
     def __init__(self, map_name: str = '3m'):
@@ -26,8 +28,9 @@ class SC2Env:
         self.env_info = self.env.get_env_info()
 
         # Number of agents and enemies
-        self.n_agents = self.env_info['n_agents']
-        self.n_enemies = self.env.n_enemies
+        self.num_agents = self.env_info['num_agents']
+        self.num_enemies = self.env.n_enemies
+        self.agents = ['agent_{}'.format(i) for i in range(self.num_agents)]
 
         # Number of actions
         self.action_shape = self.env_info['n_actions']
@@ -38,12 +41,15 @@ class SC2Env:
         self.obs_shape = self.env_info['obs_shape']
 
         # Reward and done shapes
-        self.dim_reward = self.n_agents
-        self.dim_done = self.n_agents
+        self.dim_reward = self.num_agents
+        self.dim_done = self.num_agents
 
         # Space
-        self.obs_space = (self.obs_shape, )
-        self.act_space = (self.n_actions, )
+        self.state_space = Box(-2.0, 2.0, shape=(self.state_shape, ))
+        self.obs_space = Box(-2.0, 2.0, shape=(self.obs_shape, ))
+        self.action_mask_space = Box(-2.0, 2.0, shape=(self.n_actions, ))
+        self.action_space = Discrete(self.n_actions)
+
         self.reward_space = (self.dim_reward, )
         self.done_space = (self.dim_done, )
 
@@ -51,14 +57,14 @@ class SC2Env:
         self.episode_limit = self.env_info['episode_limit']
 
         # Observation concatenation shape
-        self.obs_concate_shape = self.obs_shape + self.n_agents + self.n_actions
+        self.obs_concate_shape = self.obs_shape + self.num_agents + self.n_actions
 
         # One-hot transformations
-        self.agent_id_one_hot_transform = OneHotTransform(self.n_agents)
+        self.agent_id_one_hot_transform = OneHotTransform(self.num_agents)
         self.actions_one_hot_transform = OneHotTransform(self.n_actions)
 
         # Initialize agent IDs one-hot encoding
-        self._init_agents_id_one_hot(self.n_agents)
+        self._init_agents_id_one_hot(self.num_agents)
 
         # Buffer information
         self.buf_info = {
@@ -76,15 +82,15 @@ class SC2Env:
         """Check if the win is counted."""
         return self.env.win_counted
 
-    def _init_agents_id_one_hot(self, n_agents: int):
+    def _init_agents_id_one_hot(self, num_agents: int):
         """Initialize the one-hot encoding for agent IDs.
 
         Parameters:
-        - n_agents (int): Number of agents.
+        - num_agents (int): Number of agents.
         """
         agents_id_one_hot = [
             self.agent_id_one_hot_transform(agent_id)
-            for agent_id in range(n_agents)
+            for agent_id in range(num_agents)
         ]
         self.agents_id_one_hot = np.array(agents_id_one_hot)
 
@@ -128,14 +134,14 @@ class SC2Env:
         Returns:
         - Tuple: Tuple containing state, observation, concatenated observation, and info.
         """
-        obs, state = self.env.reset()
-        state = np.array(state)
-        obs = np.array(obs)
-        last_actions_one_hot = np.zeros((self.n_agents, self.n_actions),
+        self.env.reset()
+        obs_smac = self.env.get_obs()
+        state_smac = self.env.get_state()
+        last_actions_one_hot = np.zeros((self.num_agents, self.n_actions),
                                         dtype='float32')
         agents_id_one_hot = self._get_agents_id_one_hot()
         obs_concate = np.concatenate(
-            [obs, last_actions_one_hot, agents_id_one_hot], axis=-1)
+            [obs_smac, last_actions_one_hot, agents_id_one_hot], axis=-1)
 
         self._episode_step = 0
         self._episode_score = 0.0
@@ -143,8 +149,7 @@ class SC2Env:
             'episode_step': self._episode_step,
             'episode_score': self._episode_score,
         }
-
-        return state, obs, obs_concate, info
+        return state_smac, obs_smac, obs_concate, info
 
     def step(self, actions: Union[np.ndarray, List[int]]) -> Tuple:
         """Take a step in the environment.
@@ -160,22 +165,119 @@ class SC2Env:
         if not info:
             info = self.buf_info
 
-        obs = np.array(self.env.get_obs())
-        state = np.array(self.env.get_state())
-        reward_n = np.array([[reward] for _ in range(self.n_agents)])
+        obs_smac = self.env.get_obs()
+        state_smac = self.env.get_state()
+        reward_n = np.array([[reward] for _ in range(self.num_agents)])
 
         last_actions_one_hot = self._get_actions_one_hot(actions)
 
         self._episode_step += 1
         self._episode_score += reward
-        self.buf_info = deepcopy(info)
 
         info['episode_step'] = self._episode_step
         info['episode_score'] = self._episode_score
 
         truncated = True if self._episode_step >= self.episode_limit else False
         obs_concate = np.concatenate(
-            [obs, last_actions_one_hot, self.agents_id_one_hot], axis=-1)
+            [obs_smac, last_actions_one_hot, self.agents_id_one_hot], axis=-1)
 
-        return state, obs, obs_concate, reward_n, [terminated], [truncated
+        return state_smac, obs_smac, obs_concate, reward_n, [terminated
+                                                             ], [truncated
                                                                  ], info
+
+    def get_env_info(self):
+        """Get the environment information."""
+        env_info = {
+            'state_space': self.state_space,
+            'obs_space': self.obs_space,
+            'action_mask': self.action_mask_space,
+            'action_space': self.action_space,
+            'num_agents': self.num_agents,
+            'episode_limit': self.episode_limit,
+        }
+        return env_info
+
+
+class RLlibSMAC(MARLBaseEnv):
+
+    def __init__(self, map_name):
+        map_name = map_name if isinstance(map_name,
+                                          str) else map_name['map_name']
+        self.env = StarCraft2Env(map_name)
+
+        env_info = self.env.get_env_info()
+        self.num_agents = self.env.n_agents
+        self.agents = ['agent_{}'.format(i) for i in range(self.num_agents)]
+        obs_shape = env_info['obs_shape']
+        n_actions = env_info['n_actions']
+        state_shape = env_info['state_shape']
+        self.observation_space = Dict({
+            'obs':
+            Box(-2.0, 2.0, shape=(obs_shape, )),
+            'state':
+            Box(-2.0, 2.0, shape=(state_shape, )),
+            'action_mask':
+            Box(-2.0, 2.0, shape=(n_actions, ))
+        })
+        self.action_space = Discrete(n_actions)
+
+    def reset(self):
+        self.env.reset()
+        obs_smac = self.env.get_obs()
+        state_smac = self.env.get_state()
+        obs_dict = {}
+        for agent_index in range(self.num_agents):
+            obs_one_agent = obs_smac[agent_index]
+            state_one_agent = state_smac
+            action_mask_one_agent = np.array(
+                self.env.get_avail_agent_actions(agent_index)).astype(
+                    np.float32)
+            agent_index = 'agent_{}'.format(agent_index)
+            obs_dict[agent_index] = {
+                'obs': obs_one_agent,
+                'state': state_one_agent,
+                'action_mask': action_mask_one_agent,
+            }
+
+        return obs_dict
+
+    def step(self, actions):
+
+        actions_ls = [int(actions[agent_id]) for agent_id in actions.keys()]
+
+        reward, terminated, info = self.env.step(actions_ls)
+
+        obs_smac = self.env.get_obs()
+        state_smac = self.env.get_state()
+
+        obs_dict = {}
+        reward_dict = {}
+        for agent_index in range(self.num_agents):
+            obs_one_agent = obs_smac[agent_index]
+            state_one_agent = state_smac
+            action_mask_one_agent = np.array(
+                self.env.get_avail_agent_actions(agent_index)).astype(
+                    np.float32)
+            agent_index = 'agent_{}'.format(agent_index)
+            obs_dict[agent_index] = {
+                'obs': obs_one_agent,
+                'state': state_one_agent,
+                'action_mask': action_mask_one_agent
+            }
+            reward_dict[agent_index] = reward
+
+        dones = {'__all__': terminated}
+
+        return obs_dict, reward_dict, dones, {}
+
+    def get_env_info(self):
+        env_info = {
+            'space_obs': self.observation_space,
+            'space_act': self.action_space,
+            'num_agents': self.num_agents,
+            'episode_limit': self.env.episode_limit,
+        }
+        return env_info
+
+    def close(self):
+        self.env.close()
