02887125d066007cbdd175cf34de1cd0c50aae5b
jianzhnie <jianzhnie@126.com>
2024-04-07 16:53:55 +0800

    update marltoolkit vecenvs

--- scripts/test_multi_env.py
+++ scripts/test_multi_env.py
@@ -1,21 +1,28 @@
 import sys
 
 sys.path.append('../')
-from marltoolkit.envs.smacv1 import SMACWrapperEnv, SubprocVecSMAC
+import torch
+from torch.distributions import Categorical
 
-
-def make_envs(map_name='3m', parallels=8):
-
-    def _thunk():
-        env = SMACWrapperEnv(map_name=map_name)
-        return env
-
-    return SubprocVecSMAC([_thunk for _ in range(parallels)])
-
+from marltoolkit.envs.smacv1.smac_env import SMACWrapperEnv
 
 if __name__ == '__main__':
-    train_envs = make_envs()
-    train_envs.reset()
-    actions = train_envs.get_avail_actions()
-    print(actions)
-    train_envs.close()
+    env = SMACWrapperEnv(map_name='3m')
+    env_info = env.get_env_info()
+    print('env_info:', env_info)
+
+    action_dim = env_info['n_actions']
+    num_agents = env_info['num_agents']
+    results = env.reset()
+    print('Reset:', results)
+
+    avail_actions = env.get_available_actions()
+    print('avail_actions:', avail_actions)
+    available_actions = torch.tensor(avail_actions)
+    actions_dist = Categorical(available_actions)
+    random_actions = actions_dist.sample().numpy().tolist()
+
+    print('random_actions: ', random_actions)
+    results = env.step(random_actions)
+    print('Step:', results)
+    env.close()
--- scripts/test_multi_env2.py
+++ scripts/test_multi_env2.py
@@ -8,7 +8,7 @@ from marltoolkit.envs.smacv1.smac_env import SMACWrapperEnv
 from marltoolkit.envs.vec_env import DummyVecEnv, SubprocVecEnv
 
 
-def make_envs(map_name='3m', parallels=8):
+def make_subproc_envs(map_name='3m', parallels=8) -> SubprocVecEnv:
 
     def _thunk():
         env = SMACWrapperEnv(map_name=map_name)
@@ -18,7 +18,7 @@ def make_envs(map_name='3m', parallels=8):
                          shared_memory=False)
 
 
-def make_dummy_envs(map_name='3m', parallels=8):
+def make_dummy_envs(map_name='3m', parallels=8) -> DummyVecEnv:
 
     def _thunk():
         env = SMACWrapperEnv(map_name=map_name)
@@ -27,26 +27,48 @@ def make_dummy_envs(map_name='3m', parallels=8):
     return DummyVecEnv([_thunk for _ in range(parallels)])
 
 
-if __name__ == '__main__':
-    train_envs = make_envs()
+def test_dummy_envs():
+    parallels = 2
+    env = SMACWrapperEnv(map_name='3m')
+    env.reset()
+    env_info = env.get_env_info()
+    print('env_info:', env_info)
+
+    train_envs = make_dummy_envs(parallels=parallels)
     results = train_envs.reset()
     print('Reset:', results)
 
-    env_info = train_envs.get_env_info()
-    print('env_info:', env_info)
-    action_dim = env_info['n_actions']
-    num_agents = env_info['num_agents']
-    num_envs = 8
-
-    avail_actions = train_envs.get_available_actions()
-
+    num_envs = parallels
+    avail_actions = env.get_available_actions()
     print('avail_actions:', avail_actions)
     available_actions = torch.tensor(avail_actions)
     actions_dist = Categorical(available_actions)
-    random_actions = actions_dist.sample().numpy()
-    print('random_actions: ', random_actions)
-    print('random_actions shape: ', random_actions.shape)
+    random_actions = actions_dist.sample().numpy().tolist()
 
+    print('random_actions: ', random_actions)
+    dummy_actions = [random_actions for _ in range(num_envs)]
+    print('dummy_actions:', dummy_actions)
+    results = train_envs.step(dummy_actions)
+    print('Step:', results)
+    train_envs.close()
+
+
+def test_subproc_envs():
+    parallels = 2
+    train_envs = make_subproc_envs(parallels=parallels)
+    results = train_envs.reset()
+    print('Reset:', results)
+
+    avail_actions = train_envs.get_available_actions()
+    print('avail_actions:', avail_actions)
+    available_actions = torch.tensor(avail_actions)
+    actions_dist = Categorical(available_actions)
+    random_actions = actions_dist.sample().numpy().tolist()
+    print('random_actions: ', random_actions)
     results = train_envs.step(random_actions)
     print('Step:', results)
     train_envs.close()
+
+
+if __name__ == '__main__':
+    test_subproc_envs()
