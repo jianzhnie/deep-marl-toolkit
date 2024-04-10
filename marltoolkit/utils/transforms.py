260b273bfab5e9b4a1e382e75726782386dc8264
jianzhnie <jianzhnie@126.com>
2024-04-10 10:10:16 +0800

    update marltoolkit

--- marltoolkit/utils/transforms.py
+++ marltoolkit/utils/transforms.py
@@ -2,12 +2,17 @@ import numpy as np
 
 
 class OneHotTransform(object):
+    """One hot transform for.
 
-    def __init__(self, out_dim):
+    Args:
+        out_dim (int): The dimension of one hot vector.
+    """
+
+    def __init__(self, out_dim: int) -> None:
         self.out_dim = out_dim
 
-    def __call__(self, agent_id):
-        assert agent_id < self.out_dim
-        one_hot_id = np.zeros(self.out_dim, dtype='float32')
-        one_hot_id[agent_id] = 1.0
+    def __call__(self, index: int) -> np.ndarray:
+        assert index < self.out_dim
+        one_hot_id = np.zeros([self.out_dim])
+        one_hot_id[index] = 1.0
         return one_hot_id
